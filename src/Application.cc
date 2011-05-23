0e4f502caad87b4421d1fea8e4106d672329600d
Nathan Rajlich <nathan@tootallnate.net>
2011-05-22 18:01:38 -0700

    Finish up the '.createConnection()' function I think. It's working well at this point, and the remote and local connection functionality is working!

--- src/Application.cc
+++ src/Application.cc
@@ -1,22 +1,39 @@
 #include "Application.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 using namespace node;
 using namespace v8;
 
 namespace node_iTunes {
 
-static Persistent<String> repo_class_symbol;
+static Persistent<String> APPLICATION_CLASS_SYMBOL;
+static Persistent<String> HOST_SYMBOL;
+static Persistent<String> USERNAME_SYMBOL;
+static Persistent<String> PASSWORD_SYMBOL;
+
+static Persistent<Object> NEW_CHECKER;
 
 Persistent<FunctionTemplate> Application::constructor_template;
 
 void Application::Init(v8::Handle<Object> target) {
   HandleScope scope;
 
-  repo_class_symbol = NODE_PSYMBOL("Application");
+  // String Constants
+  APPLICATION_CLASS_SYMBOL = NODE_PSYMBOL("Application");
+  HOST_SYMBOL = NODE_PSYMBOL("host");
+  USERNAME_SYMBOL = NODE_PSYMBOL("username");
+  PASSWORD_SYMBOL = NODE_PSYMBOL("password");
 
+  // This Object is used in the Application JS constructor to ensure that the
+  // user isn't calling 'new Application', but rather '.createConnection()'.
+  NEW_CHECKER = Persistent<Object>::New(Object::New());
+
+  // Set up the 'Application' constructor template
   Local<FunctionTemplate> t = FunctionTemplate::New(New);
   constructor_template = Persistent<FunctionTemplate>::New(t);
-  constructor_template->SetClassName(repo_class_symbol);
+  constructor_template->SetClassName(APPLICATION_CLASS_SYMBOL);
   t->InstanceTemplate()->SetInternalFieldCount(1);
 
   //NODE_SET_PROTOTYPE_METHOD(t, "runSync", GetVolumeSync);
@@ -44,7 +61,7 @@ void Application::Init(v8::Handle<Object> target) {
 
   NODE_SET_METHOD(target, "createConnection", CreateConnection);
 
-  target->Set(repo_class_symbol, constructor_template->GetFunction());
+  target->Set(APPLICATION_CLASS_SYMBOL, constructor_template->GetFunction());
 }
 
 
@@ -60,6 +77,10 @@ Application::~Application() {
 
 v8::Handle<Value> Application::New(const Arguments& args) {
   HandleScope scope;
+   if (args.Length() != 1 || !NEW_CHECKER->StrictEquals(args[0]->ToObject())) {
+    return ThrowException(Exception::TypeError(String::New("Use '.createConnection()' to get an Application instance")));
+  }
+
   Application* hw = new Application();
   hw->Wrap(args.This());
   return args.This();
@@ -90,12 +111,41 @@ v8::Handle<Value> Application::SetVolumeSync(const Arguments& args) {
 v8::Handle<Value> Application::CreateConnection(const Arguments& args) {
   HandleScope scope;
 
+  if (args.Length() < 1) {
+    return ThrowException(Exception::TypeError(String::New("A callback function is required")));
+  }
+
   create_connection_request* ccr = (create_connection_request *) malloc(sizeof(struct create_connection_request));
   ccr->host = NULL;
   ccr->username = NULL;
   ccr->password = NULL;
 
-  Local<Function> cb = Local<Function>::Cast(args[0]);
+  int cbIndex = 0;
+  if (args[1]->IsFunction()) {
+    cbIndex = 1;
+    Local<Object> options = args[0]->ToObject();
+    if (options->Has(HOST_SYMBOL)) {
+      String::Utf8Value hostValue(options->Get(HOST_SYMBOL));
+      char* hostStr = (char*)malloc(strlen(*hostValue) + 1);
+      strcpy(hostStr, *hostValue);
+      ccr->host = hostStr;
+    } else {
+      return ThrowException(Exception::TypeError(String::New("The 'host' property is required when an 'options' Object is given")));
+    }
+    if (options->Has(USERNAME_SYMBOL)) {
+      String::Utf8Value unValue(options->Get(USERNAME_SYMBOL));
+      char* unStr = (char*)malloc(strlen(*unValue) + 1);
+      strcpy(unStr, *unValue);
+      ccr->username = unStr;
+    }
+    if (options->Has(PASSWORD_SYMBOL)) {
+      String::Utf8Value pwValue(options->Get(PASSWORD_SYMBOL));
+      char* pwStr = (char*)malloc(strlen(*pwValue) + 1);
+      strcpy(pwStr, *pwValue);
+      ccr->password = pwStr;
+    }
+  }
+  Local<Function> cb = Local<Function>::Cast(args[cbIndex]);
   ccr->cb = Persistent<Function>::New(cb);
 
   eio_custom(CreateConnection_Do, EIO_PRI_DEFAULT, CreateConnection_After, ccr);
@@ -108,7 +158,29 @@ v8::Handle<Value> Application::CreateConnection(const Arguments& args) {
 int CreateConnection_Do (eio_req *req) {
   NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
   struct create_connection_request * ccr = (struct create_connection_request *)req->data;
-  ccr->iTunesRef = [SBApplication applicationWithURL: [NSURL URLWithString: @"eppc://10.0.1.10/iTunes"]];
+  if (ccr->host != NULL) {
+    //printf("host: %s\n", ccr->host);
+    NSString* urlStr = @"eppc://";
+    if (ccr->username != NULL && ccr->password != NULL) {
+      //printf("username: %s\n", ccr->username);
+      //printf("password: %s\n", ccr->password);
+      NSString* unString = [NSString stringWithCString: ((const char*)ccr->username) encoding: NSUTF8StringEncoding ];
+      urlStr = [urlStr stringByAppendingString: [unString stringByAddingPercentEscapesUsingEncoding: NSASCIIStringEncoding ]];
+      urlStr = [urlStr stringByAppendingString: @":" ];
+      NSString* pwString = [NSString stringWithCString: ((const char*)ccr->password) encoding: NSUTF8StringEncoding ];
+      urlStr = [urlStr stringByAppendingString: [pwString stringByAddingPercentEscapesUsingEncoding: NSASCIIStringEncoding ]];
+      urlStr = [urlStr stringByAppendingString: @"@" ];
+    }
+    urlStr = [urlStr stringByAppendingString: [NSString stringWithCString: ((const char*)ccr->host) encoding: NSUTF8StringEncoding ]];
+    urlStr = [urlStr stringByAppendingString: @"/iTunes" ];
+    //NSLog(@"%@", urlStr);
+    NSURL* url = [NSURL URLWithString: urlStr];
+    ccr->iTunesRef = [SBApplication applicationWithURL: url];
+  } else {
+    // If no 'options' object was provided, then simply connect to the local
+    // iTunes installation. No credentials are required for this mode.
+    ccr->iTunesRef = [SBApplication applicationWithBundleIdentifier:@"com.apple.iTunes"];
+  }
   [pool drain];
   return 0;
 }
@@ -121,7 +193,9 @@ int CreateConnection_After (eio_req *req) {
   argv[0] = Local<Value>::New(Null());
 
   // We need to create an instance of the JS 'Application' class
-  Local<Object> app = Application::constructor_template->GetFunction()->NewInstance();
+  v8::Handle<Value> constructor_args[1];
+  constructor_args[0] = NEW_CHECKER;
+  Local<Object> app = Application::constructor_template->GetFunction()->NewInstance(1, constructor_args);
   Application* it = ObjectWrap::Unwrap<Application>(app);
   it->iTunesRef = ccr->iTunesRef;
   argv[1] = app;
