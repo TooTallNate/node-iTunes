7e7f5306d7931ab29d0dc8702867d20b70e0dac7
Nathan Rajlich <nathan@tootallnate.net>
2011-05-24 00:36:53 -0700

    Add another example

---
+++ examples/fluctuate-volume.js
@@ -0,0 +1,11 @@
+var iTunes = require('./');
+
+iTunes.createConnection(function(err, itunes) {
+  var i = itunes.getVolumeSync();
+  var inc = 1;
+  setInterval(function() {
+    if (i == 100) inc *= -1;
+    else if (i == 0) inc *= -1;
+    itunes.setVolumeSync(i+=inc);
+  }, 50);
+});
