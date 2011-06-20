db24a1ee2c093f936a016ce022ce1e74a5a6c3c3
Nathan Rajlich <nathan@tootallnate.net>
2011-06-19 20:12:38 -0700

    ...

--- src/js_constructors.h
+++ src/js_constructors.h
@@ -4,7 +4,7 @@ namespace node_iTunes {
 
   // All the JavaScript Class constructor functions need to be defined here.
   // This is to avoid circular dependencies with header files and specifically
-  // the "WrapInstance" function, which intelligently uses the proper JS
+  // the "Item::WrapInstance" function, which intelligently uses the proper JS
   // constuctor based on the given iTunesItem instance.
   extern v8::Persistent<v8::FunctionTemplate> application_constructor_template;
   extern v8::Persistent<v8::FunctionTemplate> item_constructor_template;
