186d8892d0fb10328f2c72f21a9dc94156785380
Nathan Rajlich <nathan@tootallnate.net>
2011-06-25 19:51:53 -0700

    Implement `Track#artworks()`.

--- src/Track.cc
+++ src/Track.cc
@@ -1,4 +1,6 @@
 #include "Track.h"
+#import "Artwork.h"
+#import "async_macros.h"
 
 using namespace node;
 using namespace v8;
@@ -21,19 +23,60 @@ void Track::Init(v8::Handle<Object> target) {
   track_constructor_template->SetClassName(TRACK_CLASS_SYMBOL);
   t->InstanceTemplate()->SetInternalFieldCount(1);
 
-  //NODE_SET_PROTOTYPE_METHOD(t, "run", GetVolume);
-  //NODE_SET_METHOD(target, "createConnection", CreateConnection);
+  NODE_SET_PROTOTYPE_METHOD(t, "artworks", Artworks);
 
   target->Set(TRACK_CLASS_SYMBOL, track_constructor_template->GetFunction());
 }
 
-// Creates a new JS "Track" instance.
+// JavaScript Constructor//////////////////////////////////////////////////////
 v8::Handle<Value> Track::New(const Arguments& args) {
   HandleScope scope;
 
   Track* track = new Track();
   track->Wrap(args.This());
   return args.This();
+}
+
+
+// Artworks ///////////////////////////////////////////////////////////////////
+// Readonly
+v8::Handle<Value> Track::Artworks(const Arguments& args) {
+  HandleScope scope;
+  REQUIRE_CALLBACK_ARG;
+  INIT(Track);
+  GET_CALLBACK;
+  BEGIN_ASYNC(EIO_Artworks, EIO_AfterArtworks);
+}
+
+int Track::EIO_Artworks(eio_req *req) {
+  INIT_EIO_FUNC;
+  iTunesTrack *item = (iTunesTrack *)ar->itemRef;
+  SBElementArray *artworks = [item artworks];
+  [artworks retain];
+  // I would normally want to move these into an NSMutableDictionary before
+  // returning to the main thread, however it seems as though these Artwork
+  // instances don't return a 'persistentID', so we have no key to cache the
+  // values off of...
+  ar->result = artworks;
+  FINISH_EIO_FUNC;
+}
+
+int Track::EIO_AfterArtworks(eio_req *req) {
+  INIT_AFTER_FUNC;
+  argv[0] = Null();
+  SBElementArray *res = (SBElementArray *)ar->result;
+  NSUInteger count = [res count];
+  Local<Array> result = Array::New(count);
+  iTunesArtwork *a;
+  for (NSUInteger i = 0; i < count; i++) {
+    a = [res objectAtIndex: i];
+    Local<Object> aWrap = artwork_constructor_template->GetFunction()->NewInstance();
+    Artwork *aUnwrap = ObjectWrap::Unwrap<Artwork>(aWrap);
+    aUnwrap->itemRef = a;
+    result->Set(Integer::New(i), aWrap);
+  }
+  argv[1] = result;
+  FINISH_AFTER_FUNC;
 }
 
 
--- src/Track.h
+++ src/Track.h
@@ -12,11 +12,16 @@ public:
 
   static v8::Handle<v8::Value> New(const v8::Arguments&);
 
+  static v8::Handle<v8::Value> Artworks(const v8::Arguments&);
+
   //static v8::Handle<v8::Value> GetContainer(const v8::Arguments&);
   //static v8::Handle<v8::Value> GetId(const v8::Arguments&);
   //static v8::Handle<v8::Value> GetName(const v8::Arguments&);
   //static v8::Handle<v8::Value> GetPersistentId(const v8::Arguments&);
 
+private:
+  static int EIO_Artworks(eio_req *);
+  static int EIO_AfterArtworks(eio_req *);
 }; // class Track
 
 } // namespace node_iTunes
