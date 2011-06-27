03624b55440800901372f1fc3fb927fe7dfe6074
Nathan Rajlich <nathan@tootallnate.net>
2011-06-27 00:23:49 -0700

    Update to artwork example to get the `data` as well.

--- examples/artwork-of-selection.js
+++ examples/artwork-of-selection.js
@@ -23,6 +23,11 @@ function onArtworks (err, artworks) {
   if (err) throw err;
   if (artworks.length > 0) {
     console.log(artworks);
+    artworks[0].data(function (err, data) {
+      if (err) throw err;
+      console.log(data.constructor.name);
+      console.log(data.length);
+    });
   } else {
     console.log('The selected track does not have any album artwork...');
   }
