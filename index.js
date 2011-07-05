f8d57b0bc2a65ff6bbbee220efcad226def8f241
Nathan Rajlich <nathan@tootallnate.net>
2011-07-04 18:53:34 -0700

    Calling 'name' on an Artwork instance causes bad for me...

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ module.exports = require('./build/default/bindings.node');
 
 var Application = module.exports.Application
   , Item = module.exports.Item
+  , Artwork = module.exports.Artwork
   , inspect = require('util').inspect
+
+// 'name' on Artwork instances is a no-no apparently...
+Artwork.prototype.name = undefined;
 
 Application.prototype.inspect = function () {
   return inspect(Application.prototype.toString.call(this));
